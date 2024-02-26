c228cae628c850c83bf034e749ee4f65abdb3f13
luid <eng.limaluis@gmail.com>
2024-02-25 22:17:02 -0300

    drone head

--- Tello/mapping.py
+++ Tello/mapping.py
@@ -79,6 +79,7 @@ def getKeyboardInput():
 def drawPoints(img, points):
     for point in points:
         cv2.circle(img, point, 5, (0,0,255), cv2.FILLED)
+        cv2.circle(img, points[-1], 8, (0,255,0), cv2.FILLED)
     cv2.putText(img, f'({(points[-1][0]-500)/100},{(points[-1][-1]-500)/100})m', (points[-1][0]+10, points[-1][1]+30), cv2.FONT_HERSHEY_PLAIN,1,(255,0,255), 1)
 
 while True:
